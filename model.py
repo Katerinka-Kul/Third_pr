de533c50d0e3689c6440140d35683c2f59a98033
onlykulkate04@gmail.com <onlykulkate04@gmail.com>
2025-10-23 10:50:33 +0500

    6 practic

--- model.py
+++ model.py
@@ -1,29 +1,47 @@
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, Optional
+from fastapi import Form
 
 class Todo(BaseModel):
-    id: int = Field(..., example=1)
-    item: str = Field(..., example="Example schema!")
+    id: Optional[int] = None  # Сделать опциональным
+    item: str = Field(..., examples=["Example schema!"])
+    
+    @classmethod
+    def as_form(
+        cls,
+        item: str = Form(...)
+    ):
+        return cls(item=item)  # id будет автоматически None
+
 class TodoItem(BaseModel):
     item: str
-    class Config:
-        schema_extra = {
-            "example": {
-            "item": "Read the next chapter of the book"
-            }
+    
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "item": "Read the next chapter of the book"
+                }
+            ]
         }
+    }
+
 class TodoItems(BaseModel):
     todos: List[TodoItem]
-    class Config:
-        schema_extra = {
-            "example": {
-                "todos": [
-                    {
-                        "item": "Example schema 1!"
-                    },
-                    {
-                        "item": "Example schema 2!"
-                    }
-                ]
-            }
+    
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "todos": [
+                        {
+                            "item": "Example schema 1!"
+                        },
+                        {
+                            "item": "Example schema 2!"
+                        }
+                    ]
+                }
+            ]
         }
+    }
